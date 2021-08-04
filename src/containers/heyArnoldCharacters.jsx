0aa7bd91e0726c5e547bc7ffba411b6ad33887cd
KARA PEDERSEN <KJPED74@GMAIL.COM>
2021-08-04 16:23:55 -0700

    added heyArnold container

---
+++ src/containers/heyArnoldCharacters.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Character from '../components/characters/Characters';
+import { fetchCharacters } from '../services/heyArnoldApi';
+
+export default class HeyArnoldCharacters extends Component {
+  state ={
+    name: '',
+    image: '',
+  };
+
+  getCharacters = async () => {
+    const { name, image } = await fetchCharacters();
+    this.setState({ name, image });
+    console.log(this.state);
+  };
+
+  componentDidMount() {
+    this.getCharacters();
+  }
+
+  render() {
+    const { name, image } = this.state;
+    return (
+      <>
+        <h1>Characters</h1>
+        <Character name={name} image={Image}
+        />
+      </>  
+    );
+  }
+}
